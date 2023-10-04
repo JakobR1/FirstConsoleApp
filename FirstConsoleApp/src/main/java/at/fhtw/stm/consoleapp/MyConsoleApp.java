2e2577f2750841b665a8d09dd9d3b3f898e9e571
Jakob Reinelt <reinelt101@gmail.com>
2023-10-04 18:59:08 +0200

    Ich weiß auch ned was ich hier tu

--- FirstConsoleApp/src/main/java/at/fhtw/stm/consoleapp/MyConsoleApp.java
+++ FirstConsoleApp/src/main/java/at/fhtw/stm/consoleapp/MyConsoleApp.java
@@ -1,2 +1,8 @@
-package at.fhtw.stm.consoleapp;public class MyConsoleApp {
+package at.fhtw.stm.consoleapp;
+
+public class MyConsoleApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("A big hello to...");
+    }
 }
