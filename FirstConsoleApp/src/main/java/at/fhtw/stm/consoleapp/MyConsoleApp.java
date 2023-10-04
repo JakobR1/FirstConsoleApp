9c9651424cf06e046b8cb26fe6501ccf21cbd983
Jakob Reinelt <reinelt101@gmail.com>
2023-10-04 19:10:08 +0200

    Ich komm gar ned mit

--- FirstConsoleApp/src/main/java/at/fhtw/stm/consoleapp/MyConsoleApp.java
+++ FirstConsoleApp/src/main/java/at/fhtw/stm/consoleapp/MyConsoleApp.java
@@ -4,5 +4,8 @@ public class MyConsoleApp {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("A big hello to...");
+
+        User user = new User();
+        User user2 = new User();
     }
 }
